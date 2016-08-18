421c0e80882b41def7ecbeec3b4462542ffa44f1
lux.lu <lux.lu@vipshop.com>
2016-08-18 15:14:14 +0800

    添加CSP-内容安全测试测试demo

--- src/server/router/index.js
+++ src/server/router/index.js
@@ -8,11 +8,23 @@
 
 function indexRouter(router){
 	router.get('/', function *(next){
-		var rows = yield dao.getArticleById('test', 1);
-		console.log(rows);
+		//var rows = yield dao.getArticleById('test', 1);
+		//console.log(rows);
+		//console.log(this.cookies);
+		/*this.response.header = {
+			'Content-Security-Policy': 'default-src *; *.mtest.vip.com'
+		};*/
+		const NONCE_STR = 'vip';
+		const RESPONSE_HEADER = {
+			'Content-Security-Policy': "default-src *; script-src mtest.vip.com:5000 'nonce-vip'; style-src mtest.vip.com:5000",
+			'X-Content-Security-Policy': 'default-src *; script-src mtest.vip.com:5000; style-src mtest.vip.com:5000',
+			'X-Webkit-CSP': 'default-src *; script-src mtest.vip.com:5000; style-src mtest.vip.com:5000'
+		};
+		this.set(RESPONSE_HEADER);
 		yield this.render('index/tmpl/index', {
 			layout: false,
 			title: '唯品会-FE首页',
+			script_nonce_str: NONCE_STR,
 			pageHeader: {
 				title: 'FEVIP'
 			}
